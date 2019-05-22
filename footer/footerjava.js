dfe60d9ea52c77ab6db37cef4040a08f492af564
Emma Blaesbjerg <emma.blaesbjerg@gmail.com>
2019-05-22 13:24:17 +0200

    Update

--- footer/footerjava.js
+++ footer/footerjava.js
@@ -1,1 +1,5 @@
 // JavaScript Document
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
--- legat/legatjava.js
+++ legat/legatjava.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+
+//Translate hjemmesiden
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}

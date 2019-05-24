a1e53e5b6b1c13ee3a05e04a01f937656231aaf8
Emma Blaesbjerg <emma.blaesbjerg@gmail.com>
2019-05-24 17:45:54 +0200

    Update

---
+++ Skelet/skeletJava.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+
+//Google translate
+
+function googleTranslateElementInit() {
+				new google.translate.TranslateElement({pageLanguage: 'da'}, 'google_translate_element');}
+
+
+
+$(document).on("click", ".toggle-text-button", function() {
+
+  // Check if text is more or less
+  if ($(this).text() == "Læs mere") {
+
+    // Change link text
+    $(this).text("Læs mindre");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide down
+    $(this).parent().children(".toggle-text").slideDown();
+    
+  } else {
+
+
+    // Change link text
+    $(this).text("Læs mere");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide up 
+    $(this).parent().children(".toggle-text").slideUp();
+    
+  }
+  });
+
+window.addEventListener("scroll",function(){
+  var target = document.getElementsByClassName('sidenav');
+  if(window.pageYOffset > 400){
+   target[0].style.display = "block";
+  }
+  else if(window.pageYOffset < 300){
+    target[0].style.display = "none";
+  }
+  }
+);
+
+
--- chatbox/chatboxjava.js
+++ chatbox/chatboxjava.js
@@ -1,13 +1,11 @@
 // JavaScript Document
-// først rammer man knappens ID som ligger i html'en, derefter tilføjer man en click + function til knappen. 
+// først rammer man knappens ID som ligger i html'en, derefter tilføjer man en click + function til knappen.
 		// når man klikker på knappen, manipulerer man med CSS'en, Strict equality (===) siger at, displayet i CSSEN SKAL være sat til none ellers fungerer functionen ikke. derefter vil man kunne åbne og lukke chatbotten, ved hjælp af css manipulation.
-
-		document.getElementById('chatBotButton').addEventListener('click', function(){
-			let container = document.getElementById('container')
-			if(container.style.display === "none"){container.style.display = 'block';}
-			else{container.style.display = "none";}
+document.getElementById('chatBotButton').addEventListener('click', function(){
+		let container = document.getElementById('container')
+		if(container.style.display === 'none') {container.style.display = "block";}
+		else {container.style.display = "none";}
     })
-	
  document.getElementById('textfelt').addEventListener('keyup' , function(event){
      if (event.keyCode === 13) {
      let text = document.getElementById('textfelt').value
@@ -20,13 +18,37 @@
 function showText(text){
     let temp = text.toUpperCase();
     if(temp.includes('HJÆLP')) {
-        return 'ligegyldig hjælp til chatbotten her'
+        return 'Prøv med et mere specefikt søgeord, så kan jeg bedre hjælpe'
     }else if(temp.includes('PRAKTIKVEJLEDER') || temp.includes('VEJLEDER')){
-        return 'du finder praktik vejlederen "indsæt navn her" et eller andet sted på skolen'
+        return 'Du finder information omkring din praktik vejlederen, på din specifikke uddannelse under fanen "Uddannelse" '
     }else if(temp.includes('PRAKTIK')){
-        return 'mega vigtig info omkring praktik her'
+        return 'Kan du være mere specifik omkring din søgning?'
     }
-    else{
+	else if(temp.includes('UDLANDET')){
+        return 'Du kan finde information om praktik i udlandet under "Generel"'
+    }
+ 	else if(temp.includes('LEGAT')){
+        return 'Du finder en masse information omkring legater og fonde under "Legat"'
+    }
+	else if(temp.includes('BUDGET')){
+        return 'Du finder en masse information omkring budget under "Legat"'
+    }
+	else if(temp.includes('ANSØGNING')){
+        return 'Du finder en masse information omkring, hvordan du skriver din ansøgning, under "Motiveret ansøgning"'
+    }
+	else if(temp.includes('VIRKSOMHED')){
+        return 'Du finder information omkring virksomheden under "Generel" eller mere specefikt under din egen uddannelse under fanen "Uddannelse"'
+    }
+	else if(temp.includes('RÅD')){
+        return 'Du finder en masse gode råd under hver fane, som er specifikke tilegnet emnet'
+    }
+	else if(temp.includes('CV')){
+        return 'Du finder information omkring CV under fanen "CV"'
+    }
+ 	else{
         return  'Det kan jeg desværre ikke lige hjælpe dig med. Prøv med "hjælp", "praktik" eller "praktikvejleder"'
     }
 }
+
+
+ 

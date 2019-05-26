c7d4d4427fb606f2353bbe139f5840801b68c4d1
Emma Blaesbjerg <emma.blaesbjerg@gmail.com>
2019-05-27 00:17:44 +0200

    Update

--- chatbox/chatboxjava.js
+++ chatbox/chatboxjava.js
@@ -2,7 +2,7 @@
 // først rammer man knappens ID som ligger i html'en, derefter tilføjer man en click + function til knappen.
 		// når man klikker på knappen, manipulerer man med CSS'en, Strict equality (===) siger at, displayet i CSSEN SKAL være sat til none ellers fungerer functionen ikke. derefter vil man kunne åbne og lukke chatbotten, ved hjælp af css manipulation.
 document.getElementById('chatBotButton').addEventListener('click', function(){
-		let container = document.getElementById('container')
+		let container = document.getElementById('container1')
 		if(container.style.display === 'none') {container.style.display = "block";}
 		else {container.style.display = "none";}
     })
--- motiveret/motiveretjava.js
+++ motiveret/motiveretjava.js
@@ -1,1 +1,6 @@
 // JavaScript Document
+
+//Translate hjemmesiden
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'da'}, 'google_translate_element');
+}

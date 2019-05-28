f8d8afc3f6768b9db74b1a9d47a01fa59bda739b
Emma Blaesbjerg <emma.blaesbjerg@gmail.com>
2019-05-28 19:19:11 +0200

    update

--- Skelet/skeletJava.js
+++ Skelet/skeletJava.js
@@ -45,3 +45,5 @@ window.addEventListener("scroll",function(){
 );
 
 
+
+
--- footer/footerjava.js
+++ footer/footerjava.js
@@ -4,3 +4,34 @@
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'da'}, 'google_translate_element');
 }
+
+var whatToClose = 0;
+	var span = document.getElementsByClassName("close")[0];
+	var modal = document.getElementById('myModal');
+	var btn = document.getElementById("WatchAgain");
+	
+	btn.onclick = function() {
+  modal.style.display = "block";
+	}
+		
+	span.onclick = function() {
+	 modal.style.display = "none";
+	}
+		
+	window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none"; 
+	  
+  }
+	}
+	function stopVideo() {
+ document.getElementById('video').muted();
+}
+	
+	window.addEventListener("scroll",function(){
+  var target = document.getElementsByClassName('modal');
+  if(window.pageYOffset > 300){
+   target[0].style.display = "block"; 
+  }
+  
+});
